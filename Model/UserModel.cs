6140cbce4041e830be4ed74fcd6f1bfc6cb33f7e
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2020-03-04 18:19:59 -0300

    Aprendendo a criar a API

---
+++ Model/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuth.Model
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
